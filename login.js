d1850b90d72cdd3dece44ff8a0b07c47d172b060
Lusia1301 <bareklusia1@gmail.com>
2024-06-21 13:08:49 +0700

    change login.js

--- login.js
+++ login.js
@@ -1,27 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const loginForm = document.getElementById('loginForm');
-    const errorElement = document.getElementById('error');
+document.getElementById("loginForm").addEventListener("submit", async function (e) {
+    e.preventDefault();
 
-    loginForm.addEventListener('submit', function(event) {
-        event.preventDefault(); // Mencegah form untuk submit secara default
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-        // Mengambil nilai email dan password dari input
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
+    const loginDetails = {
+        email: email,
+        password: password
+    };
 
-        // Simulasi permintaan ke server. Gantilah dengan permintaan sebenarnya ke server Anda.
-        const storedUser = localStorage.getItem('user');
-        const user = storedUser ? JSON.parse(storedUser) : null;
+    try {
+        const response = await fetch('https://asia-southeast2-blkkalittifaq-426014.cloudfunctions.net/blkkalittifaq/data/user', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(loginDetails)
+        });
 
-        // Memeriksa apakah email dan password cocok
-        if (user && user.email === email && user.password === password) {
-            // Jika cocok, tampilkan pesan sukses dan redirect ke halaman dashboard
-            alert('Login successful!');
-            // Ganti halaman ini dengan halaman yang sesuai setelah login sukses
+        if (response.ok) {
+            const user = await response.json();
+            // Store user information in local storage or cookie if needed
+            // Redirect to the admin dashboard
             window.location.href = 'dashboard.html';
         } else {
-            // Jika tidak cocok, tampilkan pesan error
-            errorElement.style.display = 'block';
+            document.getElementById("error").style.display = "block";
         }
-    });
+    } catch (error) {
+        console.error('Error:', error);
+        document.getElementById("error").style.display = "block";
+    }
 });
